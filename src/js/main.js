ac20199949a0e36b3b9bea58ef653c1211c24097
Bartek <kuzniewski.bar@gmail.com>
2022-04-02 14:41:11 +0200

    added footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 const navBtn = document.querySelector('.navbar__burger-icon');
-const navMenu = document.querySelector('.navbar__list')
+const navMenu = document.querySelector('.navbar__list');
+const footerYear = document.querySelector('.footer__year');
 
 navBtn.addEventListener('click', () => {
     navMenu.classList.toggle('navbar__list--active')
 })
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
