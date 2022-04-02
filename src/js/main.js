0b2abe82cfa58353518301daad9ab9ffc58d6825
Bartek <kuzniewski.bar@gmail.com>
2022-04-02 17:09:19 +0200

    added function thats close mobile nav after click on each list item

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 const navBtn = document.querySelector('.navbar__burger-icon');
 const navMenu = document.querySelector('.navbar__list');
 const footerYear = document.querySelector('.footer__year');
+const allNavItems = document.querySelectorAll('.navbar__list');
 
 navBtn.addEventListener('click', () => {
     navMenu.classList.toggle('navbar__list--active')
 })
+
+const closeMobileNav = () => {
+	allNavItems.forEach( item => {
+		item.addEventListener('click', () => {
+			navMenu.classList.remove('navbar__list--active')
+		})
+	});
+}
+
+
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
@@ -12,3 +23,4 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
+closeMobileNav();
