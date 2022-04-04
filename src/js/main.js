b1558cc9c45cccaf40c741969f14320be9aeecb8
Bartek <kuzniewski.bar@gmail.com>
2022-04-04 16:58:50 +0200

    added function that show info window on click on markers on map

--- src/js/main.js
+++ src/js/main.js
@@ -23,8 +23,8 @@ const handleCurrentYear = () => {
 function initMap() {
 	// location
 	const rolna = { lat: 52.176, lng: 21.026 };
-	const domaniewska = { lat: 52.182, lng: 21.007};
-	const centerPosition = {lat:52.177, lng: 21.016};
+	const domaniewska = { lat: 52.182, lng: 21.007 };
+	const centerPosition = { lat: 52.177, lng: 21.016 };
 
 	// map
 	const map = new google.maps.Map(document.getElementById('map'), {
@@ -33,15 +33,57 @@ function initMap() {
 	});
 
 	// markers
-	const marker = new google.maps.Marker({
+	const rolnaStreet = new google.maps.Marker({
 		position: rolna,
 		map: map,
 	});
 
-	const secondMarker = new google.maps.Marker({
+	const domaniewskaStreet = new google.maps.Marker({
 		position: domaniewska,
 		map: map,
-	})
+	});
+
+	const rolnaInfoWindow = new google.maps.InfoWindow({
+		content: `  <h4 class="contact__box-map-heading">Klinika Rolna</h4>
+		<div class="contact__box-info--map">
+		<i class="fa-solid fa-location-dot"></i>
+		<p>Warszawa, ul. Rolna 1b</p>
+	</div>
+	<div class="contact__box-info--map">
+		<i class="fa-solid fa-phone"></i>
+		<p>+48 543 233 222</p>
+	</div>
+	<div class="contact__box-info--map">
+		<i class="fa-solid fa-envelope"></i>
+		<p>rolna@dentalcare.pl</p>
+	</div>
+		`,
+	});
+
+	const domaniewskaInfoWindow = new google.maps.InfoWindow({
+		content: `<h4 class="contact__box-map-heading">Klinika Domaniewska</h4>
+		<div class="contact__box-info--map">
+		<i class="fa-solid fa-location-dot"></i>
+		<p>Warszawa, ul. Domaniewska 21</p>
+	</div>
+	<div class="contact__box-info--map">
+		<i class="fa-solid fa-phone"></i>
+		<p>+48 543 233 333</p>
+	</div>
+	<div class="contact__box-info--map">
+		<i class="fa-solid fa-envelope"></i>
+		<p>domaniewska@dentalcare.pl</p>
+	</div>
+		`,
+	});
+
+	google.maps.event.addListener(rolnaStreet, 'click', () => {
+		rolnaInfoWindow.open(map, rolnaStreet);
+	});
+
+	google.maps.event.addListener(domaniewskaStreet, 'click', () => {
+		domaniewskaInfoWindow.open(map, domaniewskaStreet);
+	});
 }
 
 handleCurrentYear();
